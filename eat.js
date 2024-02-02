cdc42139ba83a25a2b81700f6c9d9f9867822a58
AbdelrahmanShabanh <158591383+AbdelrahmanShabanh@users.noreply.github.com>
2024-02-02 17:18:56 +0200

    Add files via upload

---
+++ eat.js
@@ -0,0 +1,13 @@
+const email = document.getElementById("email");
+const password = document.getElementById("password"); 
+const form = document.getElementById("form");
+const msg = document.getElementById("msg");
+
+
+// Function to validate the email
+const validateEmail = (inputEmail)=> inputEmail.value.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+
+// Function to validate password
+const validatePassword = (inputPassword) => inputPassword.value.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+
+
